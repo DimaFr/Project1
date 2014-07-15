f59384098856bb82e01389080e82b39a803a2304
dimitry.friedman <dima.friedman@gmail.com>
2014-07-15 19:18:12 +0300

    scope does not update properly

--- projects/02_task manager/01_start/taskManager-part1-start.js
+++ projects/02_task manager/01_start/taskManager-part1-start.js
@@ -5,26 +5,68 @@
 
     var app = angular.module('TaskManager', []);
 
-    app.controller('MainController', function () {
+    app.controller('TaskMangerController', function ($scope) {
 
+        $scope.$on('addTask', function () {
+
+            $scope.$broadcast('newTask');
+        });
+        $scope.$on('editTask', function (event, data) {
+            $scope.$broadcast('oldTask', data);
+        })
 
     })
 
-    app.controller('TaskTableController', function ($scope,DataService) {
+    app.controller('TaskFormController', function ($scope, DataService) {
+        $scope.tasks = DataService.RestoreState();
+        //should pass task from model??? or use this
+        $scope.addTask = function (t) {
+            if ($scope.tasks.indexOf(t) == -1) {
+                $scope.tasks.push(t);
+                DataService.SaveState($scope.tasks);
+                $scope.$emit('addTask');
+            }
+//            DataService.SaveState(this.tasks);
+            $scope.task = {};
+        };
+        $scope.$on('oldTask', function (event, data) {
+            $scope.task = data;
+        });
+    })
+
+    app.controller('TaskTableController', function ($scope, DataService) {
 
         $scope.tasks = DataService.RestoreState();
+
+        $scope.$on('newTask', function (event, data) {
+            $scope.tasks = DataService.RestoreState();
+        });
         //listens to check box
         $scope.updateWithTask = function (task) {
             var taskIndex = $scope.tasks.indexOf(task);
             $scope.tasks[taskIndex] = task;
             DataService.SaveState($scope.tasks);
         };
+        $scope.removeTask = function (task) {
+            console.log("remove task ");
+            //remove from array
+            //find index
+            var taskIndex = $scope.tasks.indexOf(task);
+            $scope.tasks.splice(taskIndex, 1);
+            //remove from locale storage
+            DataService.SaveState($scope.tasks);
+        };
+        $scope.editTask = function (t) {
+            $scope.task = $scope.tasks[$scope.tasks.indexOf(t)];
 
+            //emit task for editing
+            $scope.$emit('editTask', $scope.task);
+        };
 
     })
     /*
-    SERVICES
-    */
+     SERVICES
+     */
     app.factory("DataService", function () {
         var _model = [];
         var _saveState = function (model) {
@@ -35,12 +77,11 @@
             _model = angular.fromJson(localStorage.dataService) || [];
             return _model;
         }
-        var DataService = {
+        return {
             model: _model,
             SaveState: _saveState,
             RestoreState: _restoreState
-        }
-        return DataService;
+        };
     });
 
 
