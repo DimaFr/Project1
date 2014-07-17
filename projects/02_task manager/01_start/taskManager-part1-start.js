143da5217863426dad22b2d59ae455b53160de11
dimitry.friedman <dima.friedman@gmail.com>
2014-07-17 18:35:29 +0300

    factrory fixed it handles tasks (inner function restoreState())

--- projects/02_task manager/01_start/taskManager-part1-start.js
+++ projects/02_task manager/01_start/taskManager-part1-start.js
@@ -6,111 +6,131 @@
     function mainController(scope) {
         scope.$on('addTask', function () {
             scope.$broadcast('newTask');
-        })
-
+        });
         scope.$on('editTask', function (event, data) {
             scope.$broadcast('oldTask', data);
-        })
-
+        });
         scope.$on('removeTask', function (event, data) {
             scope.$broadcast('deleteTask', data);
-        })
+        });
     }
 
-    function addTaskController(scope,DataService){
-//        scope.tasks=DataService.RestoreState();
+    function logController(scope) {
+        scope.logs = [];
+        scope.$on('newTask', function () {
+            scope.logs.push({
+                timeStamp: new Date(),
+                msg: "task added"
+            });
+        });
+        scope.$on('deleteTask', function () {
+            scope.logs.push({
+                timeStamp: new Date(),
+                msg: "task removed"
+            });
+        })
+
+    }
+
+    function addTaskController(scope, DataService) {
+
         scope.tasks = DataService.tasks;
-        scope.addTask = function(task){
-            if(scope.tasks.indexOf(task)==-1){
-                scope.tasks.push(task);
+        scope.addTask = function (task) {
+            if (scope.tasks.indexOf(task) == -1) {
+                scope.tasks.push({
+                    title: task.title,
+                    description: task.description,
+                    checked: false
+                });
                 //TODO:should updata dataservice first?
                 scope.$emit('addTask');
             }
-           // DataService.SaveState(scope.tasks);
             DataService.saveTasks(scope.tasks);
-            scope.task={};
+            scope.task = {};
         };
-        scope.$on('oldTask',function(event,data){
-            scope.task=data;
+        scope.$on('oldTask', function (event, data) {
+            scope.task = data;
         });
     }
 
 
     function taskTableController(scope, DataService) {
-
-//        scope.tasks = DataService.RestoreState();
         scope.tasks = DataService.tasks;
         //listens to check box
         scope.updateWithTask = function (task) {
             var taskIndex = scope.tasks.indexOf(task);
             scope.tasks[taskIndex] = task;
-//            DataService.SaveState(scope.tasks);
             DataService.saveTasks(scope.tasks)
-        }; //bind(this)
+        };
         scope.removeTask = function (task) {
-            scope.tasks.splice(scope.tasks.indexOf(task),1);
-//            DataService.SaveState(scope.tasks);
-            DataService.saveTasks();
-            scope.$emit('removeTask');
+            scope.tasks.splice(scope.tasks.indexOf(task), 1);
 
-        }
+            DataService.saveTasks(scope.tasks);
+            scope.$emit('removeTask');
+        };
         scope.editTask = function (task) {
-            //TODO: replace task with task from scope.tasks?
+
             scope.$emit('editTask', task)
         };
-        scope.$on('newTask',function(event,data){
-                //TODO: data should be updated in add controller
-//                scope.tasks = DataService.RestoreState();
+        scope.$on('newTask', function (event, data) {
                 scope.tasks = DataService.tasks;
             }
         );
-
     }
-
-
     /*
      SERVICES
      */
-//    function DataService() {
-//        var _model = [];
-//        var _saveState = function (model) {
-//            localStorage.dataService = angular.toJson(model);
-//        };
-//        var _restoreState = function () {
-//            //return saved data or empty array
-//            _model = angular.fromJson(localStorage.dataService) || [];
-//            return _model;
-//        }
-//        var DataService = {
-//            model: _model,
-//            SaveState: _saveState,
-//            RestoreState: _restoreState
-//        }
-//        return DataService;
-//    }
-function DataService(){
-    this.saveTasks = function (data) {
-        localStorage.setItem('list', JSON.stringify(data));
-    };
+    /*
+     FACTORY
+     */
 
-    this.loadTasks = function () {
-        return JSON.parse(localStorage.getItem('list'));
+    function DataService() {
+        var _model = _loadTasks() || [];
+        var _saveTasks = function (model) {
+            localStorage.dataService = angular.toJson(model);
+        };
+
+        function _loadTasks() {
+            //return saved data or empty array
+            _model = angular.fromJson(localStorage.dataService);
+            return _model;
+        }
+
+        var DataService = {
+            saveTasks: _saveTasks,
+            tasks: _model
+        }
+        return DataService;
     }
 
-    this.tasks = this.loadTasks() || [];
-}
-
+    /*
+     SERVICE
+     */
+//    function DataService() {
+//        this.saveTasks = function (model) {
+//            localStorage.dataService = angular.toJson(model);
+//        };
+//
+//        this.loadTasks = function () {
+//            return angular.fromJson(localStorage.dataService);
+//        }
+//
+//        this.tasks = this.loadTasks() || [];
+//    }
 
 
     angular.module('TaskManager', [])
         .controller({
             'MainController': ['$scope', mainController],
             'TaskTableController': ['$scope', 'DataService', taskTableController],
-            'AddTaskController':['$scope','DataService',addTaskController]
+            'AddTaskController': ['$scope', 'DataService', addTaskController],
+            'LogController': ['$scope', logController]
 
 
         })
-        .service('DataService', DataService);
+        .service('DataService', DataService)
+    //  .provider('LogProvider', LogProvider)
 
 
-})(angular, window);
+})
+(angular, window);
