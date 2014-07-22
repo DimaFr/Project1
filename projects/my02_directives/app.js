fafae7f9a956b52bc3bc3083d73a9e01c416a60f
dimitry.friedman <dima.friedman@gmail.com>
2014-07-22 16:49:55 +0300

    works on "compile pre post" template

--- projects/my02_directives/app.js
+++ projects/my02_directives/app.js
@@ -1,36 +1,68 @@
 /**
  * Created by admin on 7/21/14.
  */
-(function(){
+(function () {
 
-    var app=angular.module('app',[]);
+    var app = angular.module('app', []);
 
-    app.directive('dfInit',function(){
+    app.directive('dfInit', function () {
         return{
-            compile:function(tElement,tAttr){
+            compile: function (tElement, tAttr) {
 
                 console.log(tAttr);
                 return{
-                    pre:function(scope,e,att){
-                       scope.$eval(att.dfInit)
-                       console.log(scope.$eval(att.dfInit))
+                    pre: function (scope, e, att) {
+                        scope.$eval(att.dfInit)
+                        console.log(scope.$eval(att.dfInit))
 
                     },
-                    post:function(scope,e,att){
-
+                    post: function (scope, e, att) {
                     }
                 }
             }
         }
     });
 
-    app.directive('dfClick',function(){
-       return {
-//again $eval      scope.$apply(scope.$eval(attr.dfClick))
-//!!!!! apply - in custom directive we need to refresh scope to see the change on click
-       }
+    app.directive('dfClick', function () {
+        return {
+            compile: function (tElement, tAttr) {
+                console.log(tAttr)
+                return {
+                    pre: function (scope, e, att) {
+                        //no need to evaluate if yes - value changes
+                        //scope.$eval(att.dfClick)
+                    },
+                    post: function (scope, e, att) {
+
+                        e.on('click', function () {
+                            //change value
+                            scope.$eval(att.dfClick)
+                            //refresh to see change other scope
+                            scope.$apply();
+                        })
+                    }
+                }
+            }
+        }
+    });
+    app.directive('dfShow', function () {
+        return {
+            compile: function (tElement, tAttr) {
+                console.log(tAttr)
+                return {
+                    pre: function (scope, e, att) {
+                        scope.$eval(att.dfShow)
+                    },
+                    post: function (scope, e, att) {
+                        scope.$watch(att.dfShow, function (dfShow) {
+                            console.log(dfShow + " watch dfShow");
+                            dfShow ? e.css('visibility', 'visible') : e.css('visibility', 'hidden')
+                        })
+                    }
+                }
+            }
+        }
     });
 
-    //dfshow  use scope.watch element if show then .css visibility visible if not css.hidden
 
 }())
