bb4d065b804c6190e07acb2ecbde64e9b90356bb
dimitry.friedman <dima.friedman@gmail.com>
2014-07-30 12:01:41 +0300

    hg switch rewrite  finished

--- projects/my05_dfswitch/app.js
+++ projects/my05_dfswitch/app.js
@@ -7,7 +7,7 @@
         .directive('dfSwitch', function () {
             return {
                 restrict: 'AE',
-                require: 'dfSwitch',
+                //require: 'dfSwitch',
                 controller: ['$scope', function dfSwitchController() {
                     this.cases = [];
                 }],
@@ -15,43 +15,88 @@
                 link: function (scope, e, attr, dfSwitchController) {
                     var watchExpr = attr.dfSwitch;
 
-                    scope.$watch(watchExpr,function(value){
+                    scope.$watch(watchExpr, function (value) {
                         //recieves a, b or c
                         console.log(value);
-                        console.log(dfSwitchController.cases)
+                        //console.log(e.children());
+                        //remove previous child
+                        e.children().remove();
+
+                        //console.log(dfSwitchController.cases)
+                        //eterate array of objects with options and elements
+                        angular.forEach(dfSwitchController.cases, function (currentCase) {
+                        //compare option with switch value and append relevant as child
+                            if (currentCase.switchOption == value) {
+                                e.append(currentCase.clone);
+                            } else {
+                                if (!value && currentCase.switchOption == "") {
+                                    e.append(currentCase.clone);
+                                }
+                            }
+                        })
+
                     })
 
                 }
             }
 
         })
+
         .directive('dfSwitchWhen', function () {
             return{
-
-                transclude: 'element',
                 require: '^dfSwitch',
-
-
-                link: function (scope, e, attr, ctrl, linker) {
-                    //logs clone
-                    console.log(linker());
-                    ctrl.cases.push(linker());
-                    //logs comment
-                    console.log(e)
+                link: function (scope, e, attr, ctrl) {
+                    console.log(attr.dfSwitchWhen)
+                    //push object with option value and element to array
+                    ctrl.cases.push({switchOption: attr.dfSwitchWhen, clone: e});
                 }
             }
         })
         .directive('dfSwitchDefault', function () {
             return{
-//all should be hidden
-
                 require: '^dfSwitch',
-
-                link: function (scope, e, att, ctrl) {
-
-                  }
+                link: function (scope, e, attr, ctrl) {
+                    console.log(attr.dfSwitchDefault);
+                    //push object with option value and element to array
+                    ctrl.cases.push({switchOption: attr.dfSwitchDefault, clone: e})
+                }
             }
         })
+    /* this is version which tries to use transclude*/
+
+//        .directive('dfSwitchWhen', function () {
+//            return{
+//
+//                transclude: 'element',
+//                require: '^dfSwitch',
+//
+//
+//                link: function (scope, e, attr, ctrl, linker) {
+//                    //logs option value
+//                    console.log(attr.dfSwitchWhen)
+//                    //logs clone
+//                    console.log(linker());
+//
+//                    ctrl.cases.push({switchOption: attr.dfSwitchWhen, clone: linker()});
+//                    //logs comment
+//                    //console.log(e)
+//                }
+//            }
+//        })
+
+//        .directive('dfSwitchDefault', function () {
+//            return{
+////all should be hidden
+//                transclude: 'element',
+//                require: '^dfSwitch',
+//
+//                link: function (scope, e, attr, ctrl,linker) {
+//
+//                    console.log(attr.dfSwitchDefault);
+//                    ctrl.cases.push({switchOption: attr.dfSwitchDefault, clone: linker()})
+//                }
+//            }
+//        })
 
 
 })(window, angular);
